f659567fb28f47917f96d46b3916584acdf1cb20
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-06-11 22:30:34 -0300

    A1.3.2

--- apps/company-server/index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-console.log("Node JS!");
-
-http.createServer(async (req,res) => {
-  res.write(JSON.stringify({ message: "Hello from Node Server!" }));
-  res.end();
-})
-.listen(4000);
