bc64490c9b7a1f92e00e7bfde3fc953777ca6b79
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-06-11 21:50:30 -0300

    A1.3

--- apps/company-front/app/page.tsx
+++ apps/company-front/app/page.tsx
@@ -1,3 +1,10 @@
+"use client";
+import { httpClient } from "@devsoutinho/commons-http-client";
+
 export default function Page() {
+  httpClient
+    .get("https://api.github.com/users/omariosouto")
+    .then((data) => console.log(data))
+  
   return <h1>Hello, world!</h1>
 }
