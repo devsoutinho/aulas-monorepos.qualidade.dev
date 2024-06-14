cb0c92e0eaab58b697c4811b803886b75c40193f
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2024-06-14 00:53:22 -0300

    wip

--- apps/company-front/app/demo.test.tsx
+++ apps/company-front/app/demo.test.tsx
@@ -2,7 +2,7 @@ import { message } from "./demo";
 
 describe("demo()", () => {
   it("true must be true", () => {
-    expect(true).toBe(true);
+    expect(true).toBe(false);
   });
   it("message must be valid", () => {
     expect(message).toBe("testing");
